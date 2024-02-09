37b5685b3ee2cd9c145e499f76c096afe5af3f90
Moussa Daoud <moussa-abdillahi.daoud@polymtl.ca>
2024-02-09 17:19:09 -0500

    Jenkins test

--- src/main/java/pl/touk/sputnik/Main.java
+++ src/main/java/pl/touk/sputnik/Main.java
@@ -14,7 +14,7 @@ import pl.touk.sputnik.connector.ConnectorFacade;
 import pl.touk.sputnik.connector.ConnectorFacadeFactory;
 import pl.touk.sputnik.connector.ConnectorType;
 import pl.touk.sputnik.engine.Engine;
-
+// Jenkins Test
 public final class Main {
     private static final String SPUTNIK = "sputnik";
     private static final String HEADER = "Sputnik - review your Gerrit patchset with Checkstyle, PMD, SpotBugs and other processors!";
